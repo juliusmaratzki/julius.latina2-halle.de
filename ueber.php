9f21a5f7088da7753b4779380c46f192b7396c0a
Julius Maratzki <67836640+juliusmaratzki@users.noreply.github.com>
2021-08-18 12:04:17 +0200

    Add files via upload

---
+++ ueber.php
@@ -0,0 +1,73 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="juliusmaratzki">
+        <meta name="description" content="info - julius.latina2-halle.de">
+        <meta name="viewport" content="width=device-width, intial-scale=1.0">
+        <link rel="stylesheet" type="text/css" media="screen" href="./res/style/style.css">
+        <link rel="shortcut icon" type="image/x-icon" href="shortcut.png">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>Webseiteninformationen - julius.latina2-halle.de</title>
+    </head>
+    <body>
+
+        <div class="header">
+            Informationen zu dieser Webseite
+        </div>
+
+        <br>
+
+        <div class="T101">
+            Anbieter und verantwortlich für diese Webseite unter der Subdomain julius.latina2-halle.de ist:
+                <br><br>
+            <div class="T102">Julius Maratzki</div>
+            Franckeplatz 1, Hs. 42/43<br>
+            D-06110, Halle (Saale)<br>
+            Mailadresse: info@julius.latina2-halle.de  
+                <br><br>
+            <div class="T102">Wichtige Information vorab:</div>
+            Diese Webseite dient ausschließlich privaten und nicht kommerziellen Zwecken. Mit dieser Internetpräsenz wird kein Profit angestrebt. Daher wird auch keine Werbung geschalten und Sie werden nicht auf
+            unseriöse Domains Dritter weitergeleitet.
+                <br><br>
+            <div class="T102"><a name="genderhinweis">Genderhinweis:</a></div>
+            Aus Gründen der besseren Lesbarkeit wird auf dieser Webseite grundsätzlich nur die maskuline Form, also das generische Maskulin, verwendet. Im Sinne der Gleichbehandlung gelten entsprechende Begriffe grundsätzlich 
+            für alle Geschlechter. Die verkürzte Sprachform beinhaltet also keine Wertung, sondern hat lediglich redaktionelle Gründe.
+                <br><br>
+            <div class="T102">Hinweise zum Urheberrecht auf dieser Webseite:</div>
+            Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
+            Grenze des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors. Downloads und Kopien des Quelltextes sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte
+            auf dieser Seite nicht vom Betreiber erstellt wurden, greifen die Urheberrechte Dritter. Sollten Sie auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden
+            von Rechtsverletzungen werden wir die entsprechenden Inhalte umgehend entfernen.
+                <br><br>
+            <div class="T102">Hinweise zu Content-Management-Systemen:</div>
+            Diese Webseite wurde ohne die Verwendung von Content-Management-Systemen, also also Programmen und Plattformen, die das Bauen der eigenen Webseite erleichtern, erstellt. Der Quelltext wurde nicht von Computern,
+            sondern persönlich Zeile für Zeile erstellt. Dieser besteht aus den Auszeichnungssprachen HTML (Hypertext Markup Language) und CSS (Cascading Stylesheets) sowie den Skriptsprachen JS (Javascript) und PHP (Hypertext Processor).
+            Beide Komponenten, das Front- und das Backend, sind dabei so miteinander verknüpft, dass ein reibungsloser Ablauf der Dienste dieser Webseite garantiert ist.
+                <br><br>
+            <div class="T102">Hinweise zum Serverstandort:</div>
+            Diese Webseite wird ausschließlich auf Servern gehostet, die sich in der Bundesrepublik Deutschland befinden. Alle Daten werden sicher und den deutschen Datenschutzgesetzen entsprechend gespeichert und gesichert. Die deutschen
+            Datenschutzgesetze gehören zu den strengsten der Welt. Die Webserver befinden sich am Standort Dresden im Freistaat Sachsen. Weitere Informationen zum Hoster erhalten Sie hier.
+                <br><br>
+            <div class="T102">Webseitenversion und Buildnummer:</div>
+            Version der Webseite: 11.02.08<br>
+            Buildnummer: 11.208<br>
+            Stand der letzten Bearbeitung: 10. August 2021
+                <br><br>
+            <div class="T102">Informationen zum Client:</div>
+            Aktuelle IP-Adresse des Clients: <?php echo $_SERVER['REMOTE_ADDR'] ?> <br>
+            Aktueller Hostname des Clients: <?php echo gethostbyaddr($_SERVER['REMOTE_ADDR']); ?>
+                <br><br><br>
+            <div class="T102">Suchen Sie nach etwas Anderem?</div>
+            &nbsp; &nbsp; <a href="https://www.julius.latina2-halle.de/">→ &nbsp; Startseite</a><br>
+            &nbsp; &nbsp; <a href="https://www.julius.latina2-halle.de/impressum.html">→ &nbsp; Impressum</a><br>
+            &nbsp; &nbsp; <a href="https://www.julius.latina2-halle.de/datenschutz.html">→ &nbsp; Datenschutz</a><br>
+            &nbsp; &nbsp; <a href="https://www.julius.latina2-halle.de/changelog.html">→ &nbsp; Änderungsprotokoll</a><br>
+            &nbsp; &nbsp; <a href="https://www.julius.latina2-halle.de/kontakt.html">→ &nbsp; Kontakt</a>
+                <br><br>
+
+
+
+        </div>
+            
+    </body>
+</html>
